a0fad351486da16827b2ecf24d477a24e28b8068
Perfect-Dav <perfectadewale31@gmail.com>
2022-07-14 02:21:23 -0700

    staking

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const NFTCONTRACT = "0xacFe3CeFac34845952EaECD1a184b8c0BD70A0FD";
-export const STAKINGCONTRACT = "0x1DBEd099563c586C398016f11823385e8d089A80";
+export const NFTCONTRACT = "0x1f0dD278D62868fa58016060a1548973F8AFEd55";
+export const STAKINGCONTRACT = "0xF8C976C0c65853FE1db21E43056Fa936B88F4Be6";
 export const sonarCashApi = "https://testnet.sonar.cash/api";
-export const nftpng = "https://raw.githubusercontent.com/wenlambotoken/NFTSWenLambo/main/png/";
+export const nftpng = "ipfs://QmSifFzarzzen5Vv4TWWhpN56VksqZrF3Bmuuc4gdGTEv1/";
 
-// token contract: 0x99142F54c0D91A57370a535B8b4F3ac7137DE944;
+// token contract: 0xA85430d46863cea03fFC94c781962CE428b8eda6;
